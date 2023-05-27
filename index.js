71579864d7c2e124f5e858e9056d51a66582a414
AbdessamadPas <boughadoin@gmail.com>
2023-05-27 17:51:27 +0100

    test desploy on vercel

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-const middlewares = require('../middlewares/errors');
+const middlewares = require('./middlewares/errors');
 require('dotenv').config()
 
 
