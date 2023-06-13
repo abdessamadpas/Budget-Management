59603456f67cd1946e5ff31cf8d6a2c7208ed8e3
AbdessamadPas <boughadoin@gmail.com>
2023-06-13 21:32:34 +0100

    clean

--- services/getBlanceUser.js
+++ services/getBlanceUser.js
@@ -117,20 +117,13 @@ const getBalanceUser  = router.get('/balance/:userId',verifyToken, async (req, r
 function verifyToken(req, res, next) {
     // Get auth header value
     const bearerHeader = req.headers['authorization'];
-    // Check if bearer is undefined
     if (typeof bearerHeader !== 'undefined') {
-        // Split at the space
         const bearer = bearerHeader.split(' ');
         // Get token from array
         const bearerToken = bearer[1];
-
-        // Set the token
-
         req.token = bearerToken;
-        // Next middleware
         next();
     } else {
-        // Forbidden
         res.status(403)
         next({
             message: "Forbidden you have to login first bro😪🥱"
