51625b24380e1c862d8e2723088474a3ad0ce9a1
AbdessamadPas <boughadoin@gmail.com>
2023-08-22 12:03:31 +0100

    apply morgan just in dev env

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ app.use(express.json());
 app.use(express.static('public'))
 app.use(helmet());
 app.use(cors());
-app.use(morgan('tiny'));
+
+app.get('env') === 'development' && app.use(morgan('tiny'));
 
 // * connect to socket expense
 app.use('/sockets', expenseSocket);
